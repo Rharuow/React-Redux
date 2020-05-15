55a2d01a56b866b452df676f5a9b301a5764e6e9
Rharuow <haryssonsoares@gmail.com>
2020-05-15 17:56:44 -0300

    Cards components

--- a-simple-redux-project/src/App.js
+++ a-simple-redux-project/src/App.js
@@ -7,9 +7,12 @@ function App() {
     <div className="App">
       <h3>React Redux</h3>
       <div className="line">
-        <Card>Card</Card>
-        <Card>Card</Card>
-        <Card>Card</Card>
+        <Card title="Card 1" red>Card</Card>
+        <Card title="Card 2" blue>Card</Card>
+      </div>
+      <div className="line">
+        <Card title="Card 3" purple>Card</Card>
+        <Card title="Card 4" green>Card</Card>
       </div>
     </div>
   );
