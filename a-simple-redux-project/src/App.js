ed2645929df9947fa14e3bad5474fc3f67869bdd
Rharuow <haryssonsoares@gmail.com>
2020-05-16 20:18:39 -0300

    Interval componenet

--- a-simple-redux-project/src/App.js
+++ a-simple-redux-project/src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import './App.css';
 import Card from './components/Card';
+import Interval from './components/Interval'
 
 function App() {
   return (
     <div className="App">
       <h3>React Redux</h3>
       <div className="line">
-        <Card title="Card 1" red>Card</Card>
-        <Card title="Card 2" blue>Card</Card>
+        <Interval>
+
+        </Interval>
       </div>
       <div className="line">
+        <Card title="Card 2" blue>Card</Card>
         <Card title="Card 3" purple>Card</Card>
         <Card title="Card 4" green>Card</Card>
       </div>
